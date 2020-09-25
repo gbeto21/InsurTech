e4e98539cce89c933882b7c166d825de80ea6b63
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-25 07:49:01 -0500

    11. Data Validations were implemented.

--- components/bestOptions/network.js
+++ components/bestOptions/network.js
@@ -7,11 +7,17 @@ const httpError = require('../../utils/httpError')
 router.get('/:year', bestOptionsPerYear)
 
 function bestOptionsPerYear(req, res, next) {
-
-    let year = req.params.year
+    let year = validateParams(req.params)
     let data = controllerCreator.getBestOptions(year)
     response.success(req, res, data, 200)
+}
 
+function validateParams(params) {
+    let year = parseInt(params.year)
+    if (!year) {
+        throw httpError('Invalid format data.', 400)
+    }
+    return year
 }
 
 module.exports = router
--- components/quoteCar/network.js
+++ components/quoteCar/network.js
@@ -2,18 +2,27 @@ const express = require('express')
 const router = express.Router()
 const controllerCreator = require("./controllerCreator")
 const response = require('../response')
+const httpError = require('../../utils/httpError')
 
 router.get('/', quoteCar)
 
 function quoteCar(req, res, next) {
 
     let brand = req.query.brand
-    let year = req.query.year
+    let year = validateYear(req.query.year)
     let hasAC = (req.query.hasAC === 'true') ? true : false
 
     let data = controllerCreator.getQuoteCar(brand, year, hasAC)
     response.success(req, res, data, 200)
 
+}
+
+function validateYear(year) {
+    let yearNumer = parseInt(year)
+    if (!yearNumer) {
+        throw httpError('Invalid format data.', 400)
+    }
+    return yearNumer
 }
 
 module.exports = router
